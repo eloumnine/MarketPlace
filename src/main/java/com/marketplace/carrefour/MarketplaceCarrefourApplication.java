88239978ec1bb5a4c184ea1ea95adb677195be31
abdelhakim.eloumnine <eloumnine@gmail.com>
2025-04-30 13:26:25 +0100

    "Initial commit: Spring Boot project setup"

---
+++ src/main/java/com/marketplace/carrefour/MarketplaceCarrefourApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.carrefour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceCarrefourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceCarrefourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketplace/carrefour/MarketplaceCarrefourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.carrefour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceCarrefourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
